551298441104590dc7acf02dd93fe49a9b4dbded
cake <cakeistastydev@gmail.com>
2025-10-27 07:53:02 +0000

    3.0.12 - Registrate fix

--- src/main/java/com/cake/trading_floor/TradingFloorData.java
+++ src/main/java/com/cake/trading_floor/TradingFloorData.java
@@ -2,6 +2,7 @@ package com.cake.trading_floor;
 
 import com.cake.trading_floor.foundation.advancement.TFAdvancements;
 import com.cake.trading_floor.registry.TFRegistry;
+import com.simibubi.create.Create;
 import com.tterrag.registrate.providers.RegistrateDataProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
@@ -14,12 +15,15 @@ import java.util.concurrent.CompletableFuture;
 public class TradingFloorData {
     
     public static void gatherData(GatherDataEvent event) {
+        if (!event.getMods().contains(Create.ID))
+            return;
+
         DataGenerator generator = event.getGenerator();
         PackOutput output = generator.getPackOutput();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
         generator.addProvider(true, new TFAdvancements(output, lookupProvider));
-        event.getGenerator().addProvider(true, TradingFloor.REGISTRATE.setDataProvider(new RegistrateDataProvider(TradingFloor.REGISTRATE, TradingFloor.MOD_ID, event)));
+//        event.getGenerator().addProvider(true, TradingFloor.REGISTRATE.setDataProvider(new RegistrateDataProvider(TradingFloor.REGISTRATE, TradingFloor.MOD_ID, event)));
     }
     
 }
