aa03a8cd0f60e0a50c8c1aadf23948336a7ff135
VladisCrafter <122053125+VladisCrafter@users.noreply.github.com>
2025-11-30 12:19:41 +0300

    Introduce attached label option for Trading Depot display sources
    That required to convert both to SingleLineDisplaySource (though I don't see why this should be a problem).
    
    Also introduced itemStackAlt for TFLang to make the result look like a ValueListDisplaySource entry from base Create.

--- src/main/java/com/cake/trading_floor/content/trading_depot/displays/CurrentTradeCompletedCountDisplay.java
+++ src/main/java/com/cake/trading_floor/content/trading_depot/displays/CurrentTradeCompletedCountDisplay.java
@@ -2,25 +2,69 @@ package com.cake.trading_floor.content.trading_depot.displays;
 
 import com.cake.trading_floor.content.trading_depot.TradingDepotBlockEntity;
 import com.cake.trading_floor.foundation.TFLang;
+import com.google.common.collect.ImmutableList;
 import com.simibubi.create.api.behaviour.display.DisplaySource;
 import com.simibubi.create.content.redstone.displayLink.DisplayLinkContext;
+import com.simibubi.create.content.redstone.displayLink.source.SingleLineDisplaySource;
 import com.simibubi.create.content.redstone.displayLink.target.DisplayTargetStats;
+import com.simibubi.create.foundation.gui.ModularGuiLineBuilder;
+import com.simibubi.create.foundation.utility.CreateLang;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 
 import java.util.List;
 
-public class CurrentTradeCompletedCountDisplay extends DisplaySource {
-    
+public class CurrentTradeCompletedCountDisplay extends SingleLineDisplaySource {
+
     @Override
-    public List<MutableComponent> provideText(DisplayLinkContext context, DisplayTargetStats stats) {
+    protected MutableComponent provideLine(DisplayLinkContext context, DisplayTargetStats stats) {
         if (!(context.getSourceBlockEntity() instanceof TradingDepotBlockEntity depot))
-            return List.of();
-        
+            return null;
+
         if (depot.getLastTrade() == null)
-            return List.of(TFLang.translate("display_link.trading_depot.no_trade").component());
-        
-        return List.of(TFLang.translate("display_link.trading_depot.trades_completed")
-            .text(" " + depot.getCurrentTradeCompletedCount()).component());
+            return TFLang.translate("display_link.trading_depot.no_trade").component();
+
+        return Component.literal(String.valueOf(depot.getCurrentTradeCompletedCount()));
+    }
+
+    @Override
+    protected boolean allowsLabeling(DisplayLinkContext context) {
+        return true;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void initConfigurationWidgets(DisplayLinkContext context, ModularGuiLineBuilder builder, boolean isFirstLine) {
+        if (isFirstLine && allowsLabeling(context))
+            addLabelingTextBox(builder);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    protected void addLabelingTextBox(ModularGuiLineBuilder builder) {
+        builder.addTextInput(0, 137, (e, t) -> {
+            e.setValue("");
+            t.withTooltip(ImmutableList.of(CreateLang.translateDirect("display_source.label")
+                            .withStyle(s -> s.withColor(0x5391E1)),
+                    CreateLang.translateDirect("gui.schedule.lmb_edit")
+                            .withStyle(ChatFormatting.DARK_GRAY, ChatFormatting.ITALIC)));
+        }, "Label");
+    }
+
+    @Override
+    public List<List<MutableComponent>> provideFlapDisplayText(DisplayLinkContext context, DisplayTargetStats stats) {
+
+        if (allowsLabeling(context)) {
+            String label = context.sourceConfig()
+                    .getString("Label");
+            if (!label.isEmpty()) {
+                return ImmutableList.of(ImmutableList.of(Component.literal(label + " "), provideLine(context, stats)));
+            }
+        }
+
+        return super.provideFlapDisplayText(context, stats);
     }
     
 }
